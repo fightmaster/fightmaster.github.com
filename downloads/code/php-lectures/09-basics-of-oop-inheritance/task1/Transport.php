503ea0ff8bd3f5eb5c6cbab2b0918a8ccb2a0058
Dmitry Petrov <old.fightmaster@gmail.com>
2013-04-07 23:07:18 +0400

    Site updated at 2013-04-07 19:07:18 UTC

---
+++ downloads/code/php-lectures/09-basics-of-oop-inheritance/task1/Transport.php
@@ -0,0 +1,53 @@
+<?php
+class Transport
+{
+    /**
+     * @var string
+     */
+    public $owner;
+
+    /**
+     * @var string
+     */
+    public $model;
+
+    /**
+     * @var integer
+     */
+    public $year;
+
+    /**
+     * @var string
+     */
+    public $pilot;
+
+    /**
+     * @var string
+     */
+    public $co_pilot;
+
+    /**
+     * @var string
+     */
+    public $driver;
+
+    /**
+     * @var string
+     */
+    public $wingspan;
+
+    /**
+     * @var boolean
+     */
+    public $airbag = false;
+
+    /**
+     * @var integer
+     */
+    public $speed;
+
+    /**
+     * @var integer
+     */
+    public $accelerationTo100;
+}
