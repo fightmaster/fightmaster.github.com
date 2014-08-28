1a8be5710b419b0c6f6e84729b4569cdd0d54d11
Dmitry Petrov <dmitry.petrov@opensoftdev.ru>
2014-08-28 11:07:08 +0400

    Update blog and deploy

---
+++ deploy.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo "deploy blog"
+jekyll build
+rm -rf _site/deploy.bash
+rm -rf _deploy
+git clone git@github.com:fightmaster/fightmaster.github.com.git _deploy
+cp -R _site/* _deploy/
+cd _deploy
+git add .
+git ci -m "Site updated at $(date +%Y-%m-%d:%H:%M:%S)"
+git push origin master
+cd ../
+echo "End deploy"
